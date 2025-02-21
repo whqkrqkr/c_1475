dc4edb09dd7700abac5b927df09c609e08668af7
root <whqkrqkr12345@gmail.com>
2025-02-21 16:03:41 +0900

    c_1475

--- c_1475/main.swift
+++ c_1475/main.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+let a = readLine()!
+var n = Array(repeating: 0, count: 10)
+
+for i in a {
+    n[Int(String(i))!] += 1
+}
+
+n[9] += n[6]
+n[6] = 0
+
+if n.firstIndex(of: n.max()!) == 9 {
+    print((n[9] / 2) + (n[9] % 2))
+} else { print(n.max()!) }
